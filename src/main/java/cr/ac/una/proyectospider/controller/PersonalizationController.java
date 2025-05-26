c42c3a8cfb6749d60a896ab3d24775eac70532c4
crossac <120603305+IsaacAlvaradoMata@users.noreply.github.com>
2025-05-26 01:18:50 -0600

    [ADD] Addition of new cards

---
+++ src/main/java/cr/ac/una/proyectospider/controller/PersonalizationController.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package cr.ac.una.proyectospider.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+
+public class PersonalizationController extends Controller implements Initializable {
+
+    @FXML
+    private BorderPane root;
+    @FXML
+    private StackPane spBackgroundPersonalization;
+    @FXML
+    private ImageView imgBackgroundPersonalization;
+    @FXML
+    private Label lblTitulo;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    public void RunPersonalizationView() {
+
+
+    }
+
+    public void ResetPersonalizationView() {
+
+
+    }
+    
+}
