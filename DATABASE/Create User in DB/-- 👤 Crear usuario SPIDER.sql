cd2bdf93e95720d3137a50c9c7f202f7292c4307
gamboaxx <luis.gamboa.retana@est.una.ac.cr>
2025-06-07 18:55:25 -0600

    [UPD] DB

---
+++ DATABASE/Create User in DB/-- 👤 Crear usuario SPIDER.sql
@@ -0,0 +1,8 @@
+-- 👤 Crear usuario SPIDER con contraseña 'spider'
+CREATE USER spider IDENTIFIED BY spider
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE temp
+QUOTA UNLIMITED ON users;
+
+-- 🧠 Dar todos los privilegios (⚠️ solo recomendado en desarrollo)
+GRANT DBA TO spider;
